811a6046583f71111bc60537055d4df144c20548
Myron <myron11r@gmail.com>
2022-08-18 09:22:01 +0300

    HW3

---
+++ src/main/java/Task1/Victim.java
@@ -0,0 +1,16 @@
+package Task1;
+
+public class Victim {
+
+    public final FireDepartment fireDepartment;
+
+    public Victim(FireDepartment fireDepartment) {
+        this.fireDepartment = fireDepartment;
+    }
+
+    public void start1() {
+        synchronized (fireDepartment) {
+            fireDepartment.notifyAll();
+        }
+    }
+}
